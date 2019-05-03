fc6784ed730c971041b55a24d584e8e948d1b84c
Cokoino <37071399+Cokoino@users.noreply.github.com>
2019-05-03 23:18:55 +0800

    Add files via upload

---
+++ IR_receive_code/IR_receive_code.ino
@@ -0,0 +1,17 @@
+#include <IRremote.h>
+int RECV_PIN = 11; //Define the number port 11
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+void setup()
+ { 
+ Serial.begin(9600); //Set the serial port baud rate
+ irrecv.enableIRIn(); // Enable infrared reception
+ }
+void loop() 
+{
+if (irrecv.decode(&results)) 
+{ 
+Serial.println(results.value, HEX); //data display
+irrecv.resume();
+}
+}
